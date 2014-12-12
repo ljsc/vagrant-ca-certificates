7c77d160a947cdbd7cf23785db26b7a18d83d3ba
John Bellone <jbellone@bloomberg.net>
2014-12-12 16:53:31 -0500

    Fix name of the method for redhat capability.

--- lib/vagrant-ca-certificates/cap/redhat/update_omnibus_links.rb
+++ lib/vagrant-ca-certificates/cap/redhat/update_omnibus_links.rb
@@ -5,7 +5,7 @@ module VagrantPlugins
         # Capability for configuring the certificate bundle for Omnibus
         # Chef installation for Redhat.
         module UpdateOmnibusLinks
-          def self.update_omnibus_symlinks(m)
+          def self.update_omnibus_links(m)
             omnibus_path = m.config.omnibus_path
             trust_bundle = '/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt'
             m.communicate.sudo("ln -fsn #{trust_bundle} #{omnibus_path}/embedded/ssl/cert.pem")
