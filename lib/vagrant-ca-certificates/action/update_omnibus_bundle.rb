7de0c9ec587be755bfd580de092a2bf30378d048
John Bellone <jbellone@bloomberg.net>
2014-12-12 16:44:06 -0500

    Add notification that symlink changes are happening.

--- lib/vagrant-ca-certificates/action/update_omnibus_bundle.rb
+++ lib/vagrant-ca-certificates/action/update_omnibus_bundle.rb
@@ -10,7 +10,9 @@ module VagrantPlugins
         end
 
         def call(env)
+          ui = env[:ui]
           guest = env[:machine].guest
+          omnibus_path = env[:machine].config.omnibus_path
 
           # HACK: (jbellone) If the Omnibus installer decides to stop
           # shipping their own certificate bundles we can totally get
@@ -18,6 +20,7 @@ module VagrantPlugins
           # SSL_CERT_FILE environment variable due to not having a clean
           # way to do it globally except for /etc/environment.
           if guest.capability?(:update_omnibus_links)
+            env[:ui].info I18n.t('vagrant_ca_certificates.omnibus.update_links', path: omnibus_path)
             guest.capability(:update_omnibus_links)
           end
         end
