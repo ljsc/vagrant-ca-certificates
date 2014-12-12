1c135d2b566ff8d1713e908fb85785833e2928d1
John Bellone <jbellone@bloomberg.net>
2014-12-12 09:46:30 -0500

    Fix issues after actually testing in RHEL.
    The certificate path already exists and we upload them there. No reason
    to actually attempt to move them back into the same location.

--- lib/vagrant-ca-certificates/cap/fedora/update_certificates.rb
+++ lib/vagrant-ca-certificates/cap/fedora/update_certificates.rb
@@ -5,8 +5,6 @@ module VagrantPlugins
         # Capability for configuring the certificate bundle on Fedora.
         module UpdateCertificates
           def self.update_certificates(machine)
-            ca_certs = machine.config.ca_certificates.certs_path
-            machine.communicate.sudo("mv #{ca_certs}/* /etc/pki/ca-trust/source/anchors/")
             machine.communicate.sudo('update-ca-trust extract')
           end
         end
--- lib/vagrant-ca-certificates/cap/redhat/update_certificates.rb
+++ lib/vagrant-ca-certificates/cap/redhat/update_certificates.rb
@@ -5,12 +5,9 @@ module VagrantPlugins
         # Capability for configuring the certificate bundle on Redhat.
         module UpdateCertificates
           def self.update_certificates(machine)
-            ca_certs = machine.config.ca_certificates.certs_path
-
             # TODO: (jbellone) Fix support for older versions of RHEL. We unfortunately
             # still have a need to spin instances that are EL5 and this only seems to
             # work on EL6 and EL7.
-            machine.communicate.sudo("mv #{ca_certs}/* /etc/pki/ca-trust/source/anchors/")
             machine.communicate.sudo('update-ca-trust extract')
           end
         end
